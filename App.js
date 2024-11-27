4e203b37fe53751d7c8fcb9b29a0b0ea30d1f2af
David Leal Peláez <davidlealpelaez@gmail.com>
2024-11-27 09:38:37 -0500

    Feat: Se termina el componente base para el pomodoro

--- App.js
+++ App.js
@@ -1,31 +1,91 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Platform, TouchableOpacity, TextInput } from 'react-native';
 import Header from './src/components/Header';
+import Timer from './src/components/Timer';
+import { Audio } from 'expo-av';
 
-//const colors = ["#f7dc6f", "#a2d9ce", "#d7bde2"]
-
-const [isWorking, setIsWorking] = useState(false)
-const [time, setTime] = useState(25 * 60)
-const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK")
+const colors = ["#f7dc6f", "#a2d9ce", "#d7bde2"]
 
 export default function App() {
+
+  const [isWorking, setIsWorking] = useState(false)
+  const [time, setTime] = useState(60 * 60)
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK")
+  const [isActive, setIsActive] = useState(false)
+
+  const playSound = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/click.mp3")
+    )
+
+    await sound.playAsync()
+  }
+
+  const handleStartStop = () => {
+    playSound()
+    setIsActive(prev => !prev)
+  }
+
+  useEffect(() => {
+    let interval = null
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime(time - 1)
+      }, 1000)
+    } else {
+      clearInterval(interval)
+    }
+
+    if (time === 0) {
+      setIsActive(false)
+      setIsWorking(prev => !prev)
+      setTimeout(isWorking ? 300 : 1500)
+    }
+
+    return () => clearInterval(interval)
+  }, [isActive, time])
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Header time={time} />
-      <Text style={styles.text}>Pomodoro</Text>
-      {/* <Text style={styles.text}>{time}</Text> */}
+    <SafeAreaView style={[styles.containerArea, { backgroundColor: colors[currentTime] }]}>
+      <View style={styles.container}>
+
+        <Text style={styles.text}>Pomodoro!</Text>
+        <Header
+          currentTime={currentTime}
+          setCurrentTime={setCurrentTime}
+          setTime={setTime}
+        />
+        <Timer time={time} />
+        {/* <TextInput type="number" /> */}
+        <TouchableOpacity style={styles.constinerButton} onPress={handleStartStop}>
+          <Text style={[styles.button]}>{isActive ? "Stop" : "Start"}</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  containerArea: {
+    flex: 1
+  },
   container: {
-    flex: 1,
     marginTop: Platform.OS === "android" ? 30 : 10,
+    paddingHorizontal: 15
   },
   text: {
     fontSize: 32
+  },
+  constinerButton: {
+    backgroundColor: "#333",
+    padding: 15,
+    margingTop: 15,
+    borderRadius: 15,
+    alignItems: "center"
+  },
+  button: {
+    color: "white",
+    fontWeight: "bold"
   }
 });
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,45 @@
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 
-const Header = ({time}) => {
+const options = ["Pomodoro", "Short Break", "Long Break"]
+
+const Header = ({ setTime, currentTime, setCurrentTime }) => {
+
+  const handlePress = (index) => {
+    const newTime = index === 0 ? 60 : index === 1 ? 5 : 15;
+    setCurrentTime(index)
+    setTime(newTime * 60)
+  }
+
   return (
-    <View>{time}</View>
+    <View style={styles.container}>
+      {
+        options.map((item, index) => (
+          <TouchableOpacity
+            style={[styles.itemStyle, currentTime !== index && { borderColor: "transparent" }]}
+            key={index}
+            onPress={() => handlePress(index)}
+          >
+            <Text>{item}</Text>
+          </TouchableOpacity>
+        ))
+      }
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row"
+  },
+  itemStyle: {
+    alignItems: "center",
+    width: "33%",
+    borderWidth: 3,
+    padding: 5,
+    borderColor: "white",
+    borderRadius: 10,
+    marginVertical: 20
+  }
+})
 
 export default Header
